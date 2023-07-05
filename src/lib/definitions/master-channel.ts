3a7381e6f2bdb022801d276e0c509a415f113f73
Leonid Vinikov <czf.leo123@gmail.com>
2023-07-05 12:31:15 +0300

    Tweak: Move - GuildDataManager from `vertix-bot`

---
+++ src/lib/definitions/master-channel.ts
@@ -0,0 +1,4 @@
+/* Default Values */
+export const DEFAULT_MASTER_CATEGORY_NAME = "༄ Dynamic Channels",
+    DEFAULT_MASTER_CHANNEL_CREATE_NAME = "➕ New Channel",
+    DEFAULT_MASTER_MAXIMUM_FREE_CHANNELS = 3;
